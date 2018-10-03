b5181fd2a22812f810fa3b56f6a1d66bf7842c04
Sergey Turinskiy <sergey.turinskiy@dfsco.com>
2018-10-03 11:33:26 +0300

    Second shot

---
+++ src/app/events-app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  // tslint:disable-next-line:component-selector
+  selector: 'events-app',
+  template: `<events-list></events-list>`
+})
+export class EventsAppComponent {
+}
