c2b75d8a0ad189e558a1544ca69464e61f1a42b7
Sergey Turinskiy <sergey.turinskiy@dfsco.com>
2018-10-07 16:22:51 +0300

    Update import events in routes.ts

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,4 +1,4 @@
-import {Routes} from '@angular/router';
+import { Routes } from '@angular/router';
 import {
   EventsListComponent,
   EventDetailsComponent,
@@ -9,10 +9,10 @@ import {
 import { Error404Component } from './errors/404.component';
 
 export const appRoutes: Routes = [
-  {path: 'events/new', component: CreateEventComponent, canDeactivate: ['canDeactivateCreateEvent']},
-  {path: 'events', component: EventsListComponent, resolve: {events: EventListResolver}},
-  {path: 'events/:id', component: EventDetailsComponent, canActivate: [EventRouteActivator]},
-  {path: '404', component: Error404Component},
-  {path: '', redirectTo: '/events', pathMatch: 'full'},
-  {path: 'user', loadChildren: './user/user.module#UserModule'}
+  { path: 'events/new', component: CreateEventComponent, canDeactivate: ['canDeactivateCreateEvent'] },
+  { path: 'events', component: EventsListComponent, resolve: { events: EventListResolver } },
+  { path: 'events/:id', component: EventDetailsComponent, canActivate: [EventRouteActivator] },
+  { path: '404', component: Error404Component },
+  { path: '', redirectTo: '/events', pathMatch: 'full' },
+  { path: 'user', loadChildren: './user/user.module#UserModule' }
 ];
