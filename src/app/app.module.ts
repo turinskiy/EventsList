c6a2f797120f0719b96f5e766451cddc0dbf3573
Sergey Turinskiy <sergey.turinskiy@dfsco.com>
2018-10-07 19:00:45 +0300

    Add login form + validation required

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,7 @@ import { NavBarComponent } from './nav/nav-bar.component';
 import { ToastrService } from './common/toastr.service';
 import {appRoutes} from './routes';
 import { Error404Component } from './errors/404.component';
+import { AuthService } from './user/auth.service';
 
 @NgModule({
   imports: [
@@ -33,6 +34,7 @@ import { Error404Component } from './errors/404.component';
     Error404Component
   ],
   providers: [
+    AuthService,
     EventService,
     ToastrService,
     EventRouteActivator,
